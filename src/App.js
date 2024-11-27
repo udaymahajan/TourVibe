b0761ad56fbd0be8b3dfa68338e90e3f25a74f2a
Uday Mahajan <udaymahajan512@gmail.com>
2024-11-27 04:57:06 -0800

    Server Side Rendering Disabled

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
+import dynamic from 'next/dynamic';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/AdminDashboard";
 import SignIn from "./pages/Login";
 import SignUp from "./pages/Register";
 
+const BrowserRouter = dynamic(
+  () => import('react-router-dom').then((mod) => mod.BrowserRouter),
+  { ssr: false } // Disable server-side rendering
+);
+
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
+
 
 export default App;
